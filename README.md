b1fd398000e2c342f69edf45f16f797549a0a725
awo1f <ilyahohryakov@gmail.com>
2012-08-15 22:19:07 +0600

    First version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 jekyll-tagging-pagination
 =========================
+
+Plugin for [Jekyll](http://github.com/mojombo/jekyll) static site generator, to enable pagination of posts organized by tags.
+
+Though Jekyll provides pagination, it is only available in index pages. jekyll-tagging-pagination makes pagination also available in tag pages.
+
+Installation
+------------
+Copy `tags-pagination.rb` file into `_plugins` directory (you might need to create it before).
+
+Usage
+-----
+
+In your post's YAML front matter, add tags like this:
+
+    tags:
+    - jekyll
+    - pagination
+
+In your `_layouts` directory, create file `tag.html`. That template is used to create an index page for every tag mentioned in posts. Inside that template, use `paginator` object like this:
+
+    {% for post in paginator.post %}
+        {{ post.title }}
+    {% endfor %}
+
+`paginator` object provides tag information, use `pagination.tag` for that.
---
+++ tags-pagination.rb
@@ -0,0 +1,59 @@
+module Jekyll
+
+    class TagGenerator < Generator
+        safe true
+        
+        def generate(site)
+            if site.layouts.key? 'tag'
+                site.tags.keys.each do |tag|
+                    paginate(site, tag)
+                end
+            end
+        end
+
+        def paginate(site, tag)
+            tag_posts = site.posts.find_all {|post| post.tags.include?(tag) }
+            num_pages = TagPager.calculate_pages(tag_posts, site.config['paginate'].to_i)
+
+            (1..num_pages).each do |page|
+                pager = TagPager.new(site.config, page, tag_posts, tag, num_pages)
+                dir = File.join('tags', tag, page > 1 ? "page#{page}" : '')
+                page = TagPage.new(site, site.source, dir, tag)
+                page.pager = pager
+                site.pages << page
+            end
+        end
+    end
+
+    class TagPage < Page        
+        def initialize(site, base, dir, tag)
+            @site = site
+            @base = base
+            @dir = dir
+            @name = 'index.html'
+
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+            self.data['tag'] = tag
+            #self.data['title'] = "Posts Tagged &ldquo;"+tag+"&rdquo;"
+        end
+    end
+
+    class TagPager < Pager 
+        attr_reader :tag
+
+        def initialize(config, page, all_posts, tag, num_pages = nil)
+            @tag = tag
+            super config, page, all_posts, num_pages
+        end
+
+        alias_method :original_to_liquid, :to_liquid
+
+        def to_liquid
+            liquid = original_to_liquid
+            liquid['tag'] = @tag
+            liquid
+        end
+    end
+
+end
